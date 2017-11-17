576953633cd4827f7b5b9345bde56f839104a5fa
Flo <fh@idunas.de>
2017-11-17 12:26:41 +0100

    Added routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,6 @@
+import { ALL_COMPONENTS } from './components';
+import { ROUTES } from './app.routing';
+import { RouterModule } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -9,11 +12,12 @@ import { AppComponent } from './app.component';
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    ...ALL_COMPONENTS
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    RouterModule.forRoot(ROUTES),
     MatButtonModule,
     MatFormFieldModule,
     MatIconModule,
---
+++ src/app/components/index.ts
@@ -0,0 +1,7 @@
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
+
+export const ALL_COMPONENTS = [
+    NavbarComponent,
+    HomeComponent
+]
